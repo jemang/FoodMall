7049fc512142f03e000b276cfdd814f2ca9b8e62
tamisdaus <theskaterparadise@gmail.com>
2017-02-27 15:10:10 +0800

    try buat pages home

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  get 'pages/home'
 
   get '/signup' => 'users#new'
 
